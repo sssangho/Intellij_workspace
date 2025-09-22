56ecfd3fdd045b6789c223af809bcac4682675bc
sssangho <sangho353@naver.com>
2025-09-22 18:56:30 +0900

    Upload:게시판-MyBatis

---
+++ Post0922/src/main/java/com/du/post0922/Post0922Application.java
@@ -0,0 +1,13 @@
+package com.du.post0922;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Post0922Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Post0922Application.class, args);
+    }
+
+}
---
+++ Post0922/src/test/java/com/du/post0922/Post0922ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.du.post0922;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Post0922ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

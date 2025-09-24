02639e398ff350304f90b3ff337925aa50c8ce92
sssangho <sangho353@naver.com>
2025-09-24 19:19:06 +0900

    Upload:게시판-Thymeleaf

---
+++ Post0924/src/main/java/com/du/post0924/service/CommentService.java
@@ -0,0 +1,24 @@
+package com.du.post0924.service;
+
+
+import com.du.post0924.domain.Comment;
+import com.du.post0924.mapper.CommentMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CommentService {
+
+    private final CommentMapper commentMapper;
+
+    public List<Comment> getCommentsByPostId(Long postId) {
+        return commentMapper.findByPostId(postId);
+    }
+
+    public void addComment(Comment comment) {
+        commentMapper.insert(comment);
+    }
+}
